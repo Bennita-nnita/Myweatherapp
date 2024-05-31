09c4309ea745e44ac20127f02420fb2207812d4a
Bennita-nnita <165434996+Bennita-nnita@users.noreply.github.com>
2024-05-31 15:50:55 +0000

    search functionality using the API as well as update html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature=response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML=Math.round(temperature);
+}
+
+
+function searchCity(city){
+    let apiKey = "a4t22d03404203ac1b80ofbb2b04aaf5";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
   }
   
   let formElement = document.querySelector("form");
   formElement.addEventListener("submit", handleSearchSubmit);
   
+  searchCity("Accra")
