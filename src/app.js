77fe7692bd380b2901fe7977d250a1bf3ba3d569
Bennita-nnita <165434996+Bennita-nnita@users.noreply.github.com>
2024-05-31 18:17:57 +0000

    injected more weather data

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature=response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
     
     cityElement.innerHTML = response.data.city;
+    let currentDate = new Date();
+    timeElement.innerHTML = formatDate(currentDate);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`
     temperatureElement.innerHTML=Math.round(temperature);
 }
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+  
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+  
+    let formattedDay = days[day];
+    return `${formattedDay} ${hours}:${minutes}`;
+  }
 
 
 function searchCity(city){
