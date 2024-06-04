75f2c75f3a6080ade87f28f0d80dd37ab8558e1d
Bennita-nnita <165434996+Bennita-nnita@users.noreply.github.com>
2024-06-04 13:33:02 +0000

    inject the forecast HTML FROM JAVASCRIPT PLUS ADD CSS

--- src/app.js
+++ src/app.js
@@ -63,8 +63,35 @@ function handleSearchSubmit(event) {
     
     searchCity(searchInput.value);
   }
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let days = ["Fri", "Sat", "Sun", "Mon", "Tue"];
+    let forecastHTML = "";
+
+    days.forEach(function (day) {
+        forecastHTML += `
+        <div class="row">
+            <div class="col-2">
+                <div class="weather-forecast-date">
+                    ${day}
+                </div>
+                <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" alt="" width="36"/>
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">18°</span>
+                    <span class="weather-forecast-temperature-min">12°</span>
+                </div>
+            </div>
+        </div>`;
+    });
+    forecastElement.innerHTML = forecastHTML;
+}
   
   let formElement = document.querySelector("form");
   formElement.addEventListener("submit", handleSearchSubmit);
   
   searchCity("Accra")
+
+  displayForecast();
+  
+ 
